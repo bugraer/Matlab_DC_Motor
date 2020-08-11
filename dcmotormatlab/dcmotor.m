2a4225cdc2222c45bcafac1ca148a59e0404c860
Buğra ER <67065146+bugraer@users.noreply.github.com>
2020-08-12 00:01:49 +0300

    Add files via upload

---
+++ dcmotormatlab/dcmotor.m
@@ -0,0 +1,40 @@
+clear
+Vt=25;
+Rs=0.15;
+P_rated=100;
+V_rated = 24;
+Ra = 0.1;
+Rf = 0.8;
+Km= 0.031;
+n = 0:1300;
+w = 2*pi*n/60;
+Ia = Vt ./(Ra+Rf+Rs+Km*w);
+I_rated = P_rated/V_rated;
+figure(1);
+plot(n,Ia,'k',n,I_rated*ones(1,size(n,2)),'k--');
+ylim([0 25])
+legend('Motor akým',' Anma Akýmý');
+xlabel('motor hýzý');
+ylabel('Motor Akýmý');
+title(['Baslama Akimi', num2str(Ia(1)),'A']);
+Ea = Km * Ia .* w;
+Pout = Ia.* Ea;
+figure (2);
+plot(n,Pout, 'k',n,P_rated*ones(1,size(n,2)),'k--');
+legend('Cýkýs Gucu',' Anma Gücü');
+xlabel('motor hýzý');
+ylabel('Mekanik Cikis Gucu');
+nrated = 0;
+k=size(n,2);
+while(nrated ==0)
+    if Pout(k) >= P_rated
+        nrated = k;
+    end
+    k=k-1;
+end
+Tout = Km * Ia.^2;
+figure(3);
+plot(n,Tout,'k')
+xlabel('Motor Hýzý');
+ylabel('Motor Torku');
+title(['Baslama Torku',num2str(Tout(1)),'Nm']);
